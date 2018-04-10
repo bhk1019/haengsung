ef9f28e4eb26b1555a2682ad4e400ef777e9c4a5
Ben Kim <bhk1019@gmail.com>
2018-04-10 05:55:19 +0000

    created current sign setter method

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -5,59 +5,25 @@ class PlanetsController < ApplicationController
   def index
     @planets = Planet.all
     @current_time = Time.new
+    @planets.each do |planet|
+      planet.set_current_sign
+    end
   end
 
   def update
-    @planets = Planet.where("changes_sign_at < ?", Time.new)
-    @planets.each do |planet|
-      planet.update_attribute(:current_sign => 'Aries')
-    end
+    @planets = Planet.all
     redirect_to root_path
   end
 
-  def current_longitude_of(planet)
-    current_time = Time.new
-    year = current_time.year
-    month = current_time.month
-    day = current_time.day
-    hour = current_time.hour
-    jd = Swe4r::swe_julday(year, month, day, hour)
-    body = Swe4r::swe_calc_ut(jd, planet.swe_id, Swe4r::SEFLG_MOSEPH)
-    return body[0]
-  end
 
-  def longitude_to_sign_orb(longitude)
-    sign_orb_array = longitude.divmod(30)
-    sign_number = sign_orb_array[0]
-    case sign_number
-    when 0
-      sign_orb_array[0] = 'Aries'
-    when 1
-      sign_orb_array[0] = 'Taurus'
-    when 2
-      sign_orb_array[0] = 'Gemini'
-    when 3
-      sign_orb_array[0] = 'Cancer'
-    when 4
-      sign_orb_array[0] = 'Leo'
-    when 5
-      sign_orb_array[0] = 'Virgo'
-    when 6
-      sign_orb_array[0] = 'Libra'
-    when 7
-      sign_orb_array[0] = 'Scorpio'
-    when 8
-      sign_orb_array[0] = 'Sagittarius'
-    when 9
-      sign_orb_array[0] = 'Capricorn'
-    when 10
-      sign_orb_array[0] = 'Aquarius'
-    when 11
-      sign_orb_array[0] = 'Pisces'
-    else
-      sign_orb_array[0] = nil
-    end
-    return sign_orb_array
+
+  def changes_sign_at(planet)
+    # Algorithm
+    # Compute the sign that the planet is in at time_counter (starting at Time.new).
+    # Check if it matches the current *current_sign* in db.
+    # If not, update changes_sign_at with time_counter.
+    # If so, update time_counter accordingly and repeat.
+    # Possibly need to compute the velocity using swe4r too if I want to adjust time_counter with the appropriate step.
   end
 
 end
--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,4 +1,55 @@
 class Planet < ApplicationRecord
-  attr_accessible :current_sign
+  attr_accessor :current_sign
 
+  def set_current_sign
+    current_longitude = current_longitude_of(self)
+    @current_sign = longitude_to_sign(current_longitude)
+  end
+
+  def current_longitude_of(planet)
+    current_time = Time.new
+    year = current_time.year
+    month = current_time.month
+    day = current_time.day
+    hour = current_time.hour
+    jd = Swe4r::swe_julday(year, month, day, hour)
+    body = Swe4r::swe_calc_ut(jd, planet.swe_id, Swe4r::SEFLG_MOSEPH)
+    return body[0]
+  end
+
+  def longitude_to_sign(longitude)
+    sign_ordinal = longitude.divmod(30)
+    sign = nil
+    case sign_ordinal[0]
+    when 0
+      sign = :Aries
+    when 1
+      sign = :Taurus
+    when 2
+      sign = :Gemini
+    when 3
+      sign = :Cancer
+    when 4
+      sign = :Leo
+    when 5
+      sign = :Virgo
+    when 6
+      sign = :Libra
+    when 7
+      sign = :Scorpio
+    when 8
+      sign = :Sagittarius
+    when 9
+      sign = :Capricorn
+    when 10
+      sign = :Aquarius
+    when 11
+      sign = :Pisces
+    else
+      sign = nil
+    end
+    return sign
+  end
+
+  
 end
