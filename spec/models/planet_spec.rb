cc5c1e9f39d80336beef76e060b74f51a89006ba
Ben Kim <bhk1019@gmail.com>
2018-05-11 01:37:32 +0000

    improved card spacing and fixed wordwrap issues

---
+++ spec/models/planet_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Planet, type: :model do 
+
+  describe "changes_sign_at method" do
+    it "should return May 20 2018 when given Sun" do
+
+    end
+  end
+
+end
