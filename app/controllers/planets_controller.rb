c6f6593797504b3e4fe58fe4378fdda05c018734
Ben Kim <bhk1019@gmail.com>
2018-06-27 18:14:02 +0000

    longitude calculation now has precision to the minute

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -7,23 +7,13 @@ class PlanetsController < ApplicationController
     @current_time = Time.new
     @planets.each do |planet|
       planet.set_current_sign
+      planet.set_changes_sign_at
     end
   end
 
   def update
     @planets = Planet.all
     redirect_to root_path
-  end
-
-
-
-  def changes_sign_at(planet)
-    # Algorithm
-    # Compute the sign that the planet is in at time_counter (starting at Time.new).
-    # Check if it matches the current *current_sign* in db.
-    # If not, update changes_sign_at with time_counter.
-    # If so, update time_counter accordingly and repeat.
-    # Possibly need to compute the velocity using swe4r too if I want to adjust time_counter with the appropriate step.
   end
 
 end
--- app/models/planet.rb
+++ app/models/planet.rb
@@ -6,28 +6,13 @@ class Planet < ApplicationRecord
   end
 
   def set_changes_sign_at
-    time_step = 86400
-    changes_sign_at = Time.new
-    next_sign = self.current_sign
-    while next_sign == self.current_sign
-      next_sign = longitude_to_sign(self.longitude_at(changes_sign_at + time_step))
-      if next_sign != self.current_sign
-        time_step = 3600
-        next_sign = longitude_to_sign(self.longitude_at(changes_sign_at + time_step))
-        if next_sign != self.current_sign
-          @changes_sign_at = changes_sign_at
-          return
-        end
-        while next_sign == self.current_sign
-          next_sign = longitude_to_sign(self.longitude_at(changes_sign_at + time_step))
-          if next_sign != self.current_sign
-            @changes_sign_at = changes_sign_at
-            return
-          end
-          changes_sign_at += time_step
-        end
-      end
-      changes_sign_at += time_step
+    step = 3600
+    steps_until_change = 0
+    @changes_sign_at = Time.new
+    initial_longitude = self.current_longitude
+    until @current_sign != longitude_to_sign(initial_longitude) do  
+      @changes_sign_at += step
+      initial_longitude = longitude_at(@changes_sign_at)
     end
   end
 
@@ -35,7 +20,8 @@ class Planet < ApplicationRecord
     year = time.year
     month = time.month
     day = time.day
-    hour = time.hour
+    min_as_fraction = time.min / 60.0
+    hour = time.hour + min_as_fraction
     jd = Swe4r::swe_julday(year, month, day, hour)
     body = Swe4r::swe_calc_ut(jd, self.swe_id, Swe4r::SEFLG_MOSEPH)
     return body[0]
@@ -77,6 +63,21 @@ class Planet < ApplicationRecord
       sign = nil
     end
     return sign
+  end
+
+  def current_degree
+    current_degree = self.current_longitude.divmod(30)
+    return current_degree[1].round
+  end
+
+  def next_sign
+    return longitude_to_sign(self.current_longitude + 30)
+  end
+
+  def is_retrograde?
+    now = Time.new
+    one_minute_from_now = now + 60
+    return self.longitude_at(now) > self.longitude_at(one_minute_from_now)
   end
 
 end
