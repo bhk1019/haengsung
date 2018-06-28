fea55040ca02351e1ec50861ef9a3577b272dc4a
Ben Kim <bhk1019@gmail.com>
2018-06-28 17:24:24 +0000

    added is retrograde attribute to planet

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328062851) do
+ActiveRecord::Schema.define(version: 20180628172335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180328062851) do
     t.datetime "updated_at", null: false
     t.datetime "changes_sign_at"
     t.string "current_sign"
+    t.boolean "is_retrograde"
   end
 
 end
